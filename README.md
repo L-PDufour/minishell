4fd462369a4e2f89d841c648c50299dbb3798d4d
ldufour <leonpierre.dufour@gmail.com>
2023-12-04 17:02:52 -0500

    Pseudo token

--- README.md
+++ README.md
@@ -1,38 +1,36 @@
 <h1 align="center">minishell</h1>
 1- create propmt:
 
-    the form will be (pwd) (user) ($) (cmd + option)
+- [ ] the form will be (pwd) (user) ($) (cmd + option)
 
 2- Parsing command into table:
 
-    read the prompt
-    parse the parse right after the ($)
-    split the (cmd with spaces and save it an array)
+- [ ] read the prompt
+- [ ] parse the parse right after the ($)
+- [ ] split the (cmd with spaces and save it an array)
 
 3- create builtin:
 
-    echo(read the line and print it in the next line of stdoutput)
-    cd(and cd options)
-    pwd(current path)
-    env() 4- implement shortcuts:
-    ctr + c
-    ctr + d
-    ctr + \
-    possibility to add arrows (up and down) to navigate in the history table (optional)
-    possibility to add arrows (left and right) to move the prompt cursor (optional)
+- [ ] echo(read the line and print it in the next line of stdoutput)
+- [ ] cd(and cd options)
+- [ ] pwd(current path)
+- [ ] env() 4- implement shortcuts:
+- [ ] ctr + c
+- [ ] ctr + d
+- [ ] ctr + \
+- [ ] possibility to add arrows (up and down) to navigate in the history table (optional)
+- [ ] possibility to add arrows (left and right) to move the prompt cursor (optional)
 
 5- implement the pipes with heredoc
 ## TODO
 
-- [ ] Do a makefile
-- [ ] Choose between or libft
-- [ ] Install readline library
-- [ ] Resume the articles
-- [ ] Resume the function
+- [x] Do a makefile
+- [x] Choose between or libft
+- [x] Install readline library
+- [\] Read the articles
+- [\] Read the function
 - [ ] Parsing
-    - [ ] Choose an algo for parsing
-    - [ ] Create a small demo
-    - [ ] Present the demo
+- [L] Grammar and lexical rules of bash
 - [ ] Execution
 - [ ] Builtin at the end
 - [ ] Faire un tester avec bash et voir la dif entre ton shell et bash avec un fichier history   
--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: ldufour <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 08:37:27 by ldufour           #+#    #+#             */
-/*   Updated: 2023/11/30 13:04:21 by ldufour          ###   ########.fr       */
+/*   Updated: 2023/12/06 13:34:30 by ldufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <readline/readline.h>
 #include <unistd.h>
 
 bool	ft_iswhitespace(int c)
@@ -48,27 +49,88 @@ char	**parsing_arguments(char *argv)
 // 	exit_pipex(-1, "Can't execute child process 1", pipex);
 // }
 //
-int	main(int argc, char **argv)
+// void ft_isspecial(char c)
+// {
+//     const char *special_chars = "|<>\"'";
+//     
+//     if (ft_strchr(special_chars, c) != NULL)
+//         printf("%c is a special character\n", c);
+// }
+//
+void ft_isspecial(int c)
 {
-	int pipe[2];
+	if (c == PIPE)
+		printf("PIPE");
+	else if (c == REDIR_I)
+		printf("REDIR_I");
+	else if (c == REDIR_O)
+		printf("REDIR_O");
+	else if (c == SPACE)
+		printf("SPACE");
+	else if (c == TAB)
+		printf("TAB");
+	else if (c == NEWLINE)
+		printf("NEWLINE");
+	else if (c == SINGLE_QUOTE)
+		printf("SINGLE_QUOTE");
+	else if (c == DOUBLE_QUOTE)
+		printf("DOUBLE_QUOTE");
+	else if (c == BACKSLASH)
+		printf("BACKSLASH");
+	else if (ft_isalpha(c))
+		printf("STRING");
+	// Add more conditions for other defines as needed
+	// else
+		// printf("Not a special character");
+}
 
-	argc = 0;
+int	getToken(char *str)
+{
+	int	i;
+	int	type;
+
+	type = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (ft_iswhitespace(str[i])) // avancer la str en cas d'espace
+			i++;
+		ft_isspecial(str[i]);
+		i++;	
+	}
+	return (type);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		pipe[2];
+	int		childPid;
+	char	*cmd_line;
+	char	**cmd_table;
+	pid_t	Pid;
+
+	// argc = 0;
 	// https://www.cs.cornell.edu/courses/cs414/2004su/homework/shell/shell.html
 	while (1)
 	{
-		int childPid;
-		char *cmdline;
 		printf("test>");
-		cmdline = readline("");
-		cmdline = *parsing_arguments(*argv);
-		if (cmdline)
+		cmd_line = readline("");
+		// cmd_line = *parsing_arguments(*argv);
+		if (cmd_line)
 		{
-		childPid = fork();
-			if (childPid == 0)
-			{
-
-			}
+			// add_history(cmd_line);
+			getToken(cmd_line);
+			// scan token
+			// next token
+			printf("\n");
+			free(cmd_line);
 		}
 	}
+	wait(NULL);
 }
-
+// https://www.youtube.com/watch?v=SToUyjAsaFk&list=PLGU1kcPKHMKj5yA0RPb5AK4QAhexmQwrW&index=19
+// Regarder pour la creation d'un recursive descent parser
+//
+// Creer des regles
+// cmd_table[0] doit etre une commande
+// si < ou > ca foit etre un filename qui suit
