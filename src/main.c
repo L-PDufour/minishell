47a4c3217412df9c6b777bdb1cfda49f4405666a
Leon-Pierre Dufour <ldufour@c1r4p5.42quebec.com>
2023-12-11 14:21:46 -0500

    small push

--- src/main.c
+++ src/main.c
@@ -30,32 +30,31 @@ void	*safe_calloc(size_t nmemb, size_t size)
 	return (ret);
 }
 
-
-void	getToken(char *str,int i, t_list *head)
+void	getToken(char *str, int i, t_list *head)
 {
 }
-//TODO Je passe ma cmd, je la remplis et apres je la rajoute a ma linked list
-//Dois penser a proteger si NULL pour ne pas derefencer un pointer NULL
+// TODO Je passe ma cmd, je la remplis et apres je la rajoute a ma linked list
+// Dois penser a proteger si NULL pour ne pas derefencer un pointer NULL
 void	recursive_tokenizer(char *str, int i, t_list **head)
 {
-	t_cmd *cmd;
-	t_list *node;
+	t_cmd	*cmd;
+	t_list	*node;
+
 	if (!str || str[i] == '\0')
 		return ;
 	node = ft_lstnew(cmd);
 	getToken(str, i, *head);
 	ft_lstadd_front(head, (void *)cmd);
-
 	recursive_tokenizer(str, i, head);
 	return ;
 }
 
-//TODO Je crois que cette fonction sert à rien
+// TODO Je crois que cette fonction sert à rien
 t_list	**init_struct(void)
 {
-	// static t_cmd	*command;
 	static t_list	**head;
 
+	// static t_cmd	*command;
 	// command = safe_calloc(1, sizeof(t_cmd));
 	head = safe_calloc(1, sizeof(t_list *));
 	// head = ft_lstnew(command);
@@ -70,7 +69,12 @@ int	main(int argc, char **argv, char **envp)
 
 	cmd_line = "";
 	i = 0;
+	if (argc == 2)
+		cmd_line = argv[1]; // Use argv[1] for the first command-line argument
+	else
+		cmd_line = readline("");
 	head = init_struct();
+	printf("%s\n", cmd_line);
 	recursive_tokenizer(cmd_line, i, head);
 	// ((t_cmd *)head->content)->infile = strdup("test");
 	// printf("%s\n", ((t_cmd *)head->content)->infile);
