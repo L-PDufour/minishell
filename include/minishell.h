807ab96fe606f007d46488c0a5d27112096756f8
ldufour <leonpierre.dufour@gmail.com>
2023-12-12 08:58:28 -0500

    lexer

--- include/minishell.h
+++ include/minishell.h
@@ -6,35 +6,35 @@
 /*   By: ldufour <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 08:39:58 by ldufour           #+#    #+#             */
-/*   Updated: 2023/12/11 08:46:11 by ldufour          ###   ########.fr       */
+/*   Updated: 2023/12/12 08:40:35 by ldufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-# include "../libft/libft.h"
-# include <fcntl.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <stdbool.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <unistd.h>
-# include <sys/wait.h>
+#include "../libft/libft.h"
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-# define PIPE 124        // |
-# define REDIR_I 60      // <
-# define REDIR_O 62      // >
-# define APPEND
-# define HEREDOC
+#define PIPE 124   // |
+#define REDIR_I 60 // <
+#define REDIR_O 62 // >
+#define APPEND
+#define HEREDOC
 // # define SPACE 32        // ' '
 // # define TAB 9           // '\t'
 // # define NEWLINE 10      // '\n'
-# define SINGLE_QUOTE 39 // '
-# define DOUBLE_QUOTE 34 // "
-# define BACKSLASH 92    // \
+#define SINGLE_QUOTE 39 // '
+#define DOUBLE_QUOTE 34 // "
+#define BACKSLASH                                                              \
+  92 // \
 # define DOLLAR      36  // $
 
 // TODO enum et norminette
@@ -43,31 +43,31 @@
 // Commencer par executer une commande
 // Developpper l'intelligence ensuite
 
-typedef enum TokenType
-{
+typedef enum TokenType {
 
-	COMMAND_T,
-	REDIR_INPUT_T,
-	REDIR_OUTPUT_T,
-	REDIR_AMEND_T,
-	PIPE_T,
-	HERE_DOC_T,
+  COMMAND_T,
+  REDIR_INPUT_T,
+  REDIR_OUTPUT_T,
+  REDIR_AMEND_T,
+  PIPE_T,
+  HERE_DOC_T,
 
-}				TokenType;
+} TokenType;
 
-typedef struct s_cmd
-{
-	TokenType	token_type;
-	char		**cmd_table;
-	int			fd_input;
-	int			fd_output;
-	char		*outfile;
-	char		*infile;
-	int			pipe[2];
-}	t_cmd;
+typedef struct s_cmd {
+  TokenType token_type;
+  char **cmd_table;
+  int fd_input;
+  int fd_output;
+  char *outfile;
+  char *infile;
+  int pipe[2];
+} t_cmd;
 // struct t_list chat = {};
-bool			ft_iswhitespace(int c);
 
-bool	ft_iswhitespace(int c);
-
+bool ft_iswhitespace(int c);
+// Lexer.c
+void getToken(char *str, int i, t_list *head);
+void recursive_tokenizer(char *str, int i, t_list **head);
+void quotes_parser(char *str, int c);
 #endif
--- src/main.c
+++ src/main.c
@@ -6,79 +6,62 @@
 /*   By: ldufour <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Updated: 2023/12/08 16:12:08 by ldufour          ###   ########.fr       */
-/*   Updated: 2023/12/11 08:47:42 by ldufour          ###   ########.fr       */
+/*   Updated: 2023/12/12 08:57:12 by ldufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	exit_prg_at_error(char *str)
-{
-	printf("Error\n");
-	if (str)
-		printf("%s\n", str);
-	exit(EXIT_FAILURE);
+bool ft_iswhitespace(int c) {
+  if (c == 32 || (c >= 9 && c <= 13))
+    return (true);
+  return (false);
 }
 
-void	*safe_calloc(size_t nmemb, size_t size)
-{
-	void	*ret;
-
-	ret = ft_calloc(nmemb, size);
-	if (!ret)
-		exit_prg_at_error("Malloc failure");
-	return (ret);
+void exit_prg_at_error(char *str) {
+  printf("Error\n");
+  if (str)
+    printf("%s\n", str);
+  exit(EXIT_FAILURE);
 }
 
-void	getToken(char *str, int i, t_list *head)
-{
-}
-// TODO Je passe ma cmd, je la remplis et apres je la rajoute a ma linked list
-// Dois penser a proteger si NULL pour ne pas derefencer un pointer NULL
-void	recursive_tokenizer(char *str, int i, t_list **head)
-{
-	t_cmd	*cmd;
-	t_list	*node;
+void *safe_calloc(size_t nmemb, size_t size) {
+  void *ret;
 
-	if (!str || str[i] == '\0')
-		return ;
-	node = ft_lstnew(cmd);
-	getToken(str, i, *head);
-	ft_lstadd_front(head, (void *)cmd);
-	recursive_tokenizer(str, i, head);
-	return ;
+  ret = ft_calloc(nmemb, size);
+  if (!ret)
+    exit_prg_at_error("Malloc failure");
+  return (ret);
 }
 
-// TODO Je crois que cette fonction sert à rien
-t_list	**init_struct(void)
-{
-	static t_list	**head;
+// TODO: Je crois que cette fonction sert à rien
+t_list **init_struct(void) {
+  static t_list **head;
 
-	// static t_cmd	*command;
-	// command = safe_calloc(1, sizeof(t_cmd));
-	head = safe_calloc(1, sizeof(t_list *));
-	// head = ft_lstnew(command);
-	return (head);
+  // static t_cmd	*command;
+  // command = safe_calloc(1, sizeof(t_cmd));
+  head = safe_calloc(1, sizeof(t_list *));
+  // head = ft_lstnew(command);
+  return (head);
 }
+int main(int argc, char **argv, char **envp) {
+  char *cmd_line;
+  t_list **head;
+  static int i;
 
-int	main(int argc, char **argv, char **envp)
-{
-	char		*cmd_line;
-	t_list		**head;
-	static int	i;
-
-	cmd_line = "";
-	i = 0;
-	if (argc == 2)
-		cmd_line = argv[1]; // Use argv[1] for the first command-line argument
-	else
-		cmd_line = readline("");
-	head = init_struct();
-	printf("%s\n", cmd_line);
-	recursive_tokenizer(cmd_line, i, head);
-	// ((t_cmd *)head->content)->infile = strdup("test");
-	// printf("%s\n", ((t_cmd *)head->content)->infile);
-	return (0);
+  cmd_line = "";
+  i = 0;
+  if (argc == 2)
+    cmd_line = argv[1]; // Use argv[1] for the first command-line argument
+                        // else
+  quotes_parser(argv[1], '"');
+  // cmd_line = readline("");
+  // head = init_struct();
+  // printf("%s\n", cmd_line);
+  // recursive_tokenizer(cmd_line, i, head);
+  // ((t_cmd *)head->content)->infile = strdup("test");
+  // printf("%s\n", ((t_cmd *)head->content)->infile);
+  return (0);
 }
 
 // Pseudo code
